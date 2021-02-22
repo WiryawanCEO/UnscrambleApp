655a70529d1c2d5ed47e34086dcd417333e580fd
wiryawanceo <wiryawan_wicaksono@live.com>
2021-02-22 10:19:19 +0700

    Start to implement viewModel class and adding logging for better understanding of VieModel lifecycles.

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.android.unscramble.ui.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+    }
+
+    private var _score = 0
+    val score: Int
+    get() = _score
+
+    private var _currentWordCount = 0
+    val currentWordCount: Int
+    get() = _currentWordCount
+
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+    get() = _currentScrambledWord
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+}
